95504dd32773f7bf8a97486143e15cd664875130
DIKSINTH <diksinth1@gmail.com>
2025-11-10 17:22:46 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/Header.jsx";
-import SustainableFutureSection from "../components/SustainableFutureSection.jsx";
 import ImageSlider from "../components/ImageSlider.jsx";
 import ClientLogosSection from "../components/ClientLogosSection.jsx";
 import PlumbingExcellenceSection from "../components/PlumbingExcellenceSection.jsx";
@@ -16,7 +15,6 @@ function Home() {
   return (
     <div>
       <Header />
-      <SustainableFutureSection />
       <ImageSlider />
       <ClientLogosSection />
       <PlumbingExcellenceSection />
