c71413209cda3c1e1f4c846c6781894a3abf97f7
DIKSINTH <diksinth1@gmail.com>
2025-11-13 12:30:05 +0530

    updated

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/ProductDetailsComponents/Header.jsx";
 import ProductDetail from "../components/ProductDetailsComponents/ProductDetail.jsx";
-import ProductShowcase from "../components/ProductDetailsComponents/ProductShowCase.jsx";
+import ProductShowcase from "../components/ProductDetailsComponents/ProductShowcase.jsx";
 import IndustryExpertService from "../components/ProductDetailsComponents/IndustryExpertService.jsx";
 import SKSFooter from "../components/SKSFooter.jsx";
 
